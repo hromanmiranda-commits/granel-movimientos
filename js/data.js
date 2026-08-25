d8ca5eb0a66cad513ac97562f626b6e65424dfaf
Hector Roman <hromanmiranda@gmail.com>
2026-08-25 01:04:17 -0400

    Actualización de datos Granel Movimientos 2026-08-25 01:04

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
-// Granel Movimientos Data Source (Auto-updated: 2026-08-25 00:56:56)
+// Granel Movimientos Data Source (Auto-updated: 2026-08-25 01:04:17)
 window.GRANEL_DATA = {
     kpis: {
         comprasLitros: 68985.0,
